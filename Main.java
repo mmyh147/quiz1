3d83d7affdd7bb7da125bceb08ce154a0ff57b98
mmyh147 <61750916+mmyh147@users.noreply.github.com>
2024-02-19 11:25:45 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //  ============== Task 1 ===================
+//        int num1, num2, num3;
+//        System.out.println("Enter the first number: ");
+//        num1 = input.nextInt();
+//        System.out.println("Enter the second number: ");
+//        num2 = input.nextInt();
+//        System.out.println("Enter the third number: ");
+//        num3 = input.nextInt();
+//        if(num1 > num2){
+//            if(num1 > num3){
+//                System.out.println("The largest number: " + num1);
+//            }else {
+//                System.out.println("The largest number: " + num3);
+//            }
+//        }else{
+//            if (num2 > num3){
+//                System.out.println("The largest number: " + num2);
+//            }else {
+//                System.out.println("the largest number: " + num3);
+//            }
+//        }
+
+        // ============== Task 2 ================
+//        System.out.println("Enter the String: ");
+//        String s = input.nextLine();
+//        System.out.println("Enter the number: ");
+//        int num = input.nextInt();
+//        System.out.println(s.charAt(num));
+
+
+        // ============ task 3 =================
+//        int  sum = 0;
+//        String num1 = "0";
+//        do {
+//            sum +=Integer.parseInt(num1);
+//            System.out.println("Enter the number and enter exit if you finish: ");
+//            num1 = input.nextLine();
+//
+//        }while (!num1.equalsIgnoreCase("exit"));
+//        System.out.println("The sum: " + sum);
+
+
+        // ========== Task 4 =============
+//        int [] numbers = {10, -21, 30, 31, -25};
+//
+//        for( int n : numbers){
+//            if(n > 0){
+//                System.out.println(n + " is a positive number ");
+//            } else if (n < 0) {
+//                System.out.println(n + " is a negative number ");
+//            }else{
+//                System.out.println(n + " is a zero ");
+//            }
+//        }
+
+        // ================ Task 5 ==================
+//        String [] words = {"Tuwaiq", "Bootcamp", "Stuedent", "JAVA"};
+//        String shortest= words[0];
+//        for (String s: words){
+//            if(s.length() < shortest.length()){
+//                shortest = s;
+//            }
+//        }
+//
+//        System.out.println("The shortest: " + shortest);
+
+
+
+    }
+
+
+
+
+
+}
